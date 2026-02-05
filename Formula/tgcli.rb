26addab20f8f94d0d8d10171c3b6a0c79a6f0b24
Dario <me@dgrp.es>
2026-02-05 12:02:50 +0400

    Update to v0.2.0

--- Formula/tgcli.rb
+++ Formula/tgcli.rb
@@ -1,34 +1,45 @@
 class Tgcli < Formula
   desc "Telegram CLI tool using grammers (pure Rust MTProto)"
   homepage "https://github.com/dgrr/tgcli"
-  version "0.1.1"
+  version "0.2.0"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/dgrr/tgcli/releases/download/v#{version}/tgcli-darwin-arm64"
-      sha256 "07e3d75a2425a8c2987e49393811ea2f884755668a234e9191b4ed4bbe102c50"
+      sha256 "1378f4f2842037a5e70096d514f925d2d7546186903699bd9985dcd11c5feabc"
     end
     on_intel do
       url "https://github.com/dgrr/tgcli/releases/download/v#{version}/tgcli-darwin-amd64"
-      sha256 "bbd9e52b2acc000404e8ee47ab1fe71d2f59035bab67a7de46108161732bf1cd"
+      sha256 "d30e1d85103594839e72cc53e5f8b5d5fcab15130da7e18e509d471c06ac38c2"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/dgrr/tgcli/releases/download/v#{version}/tgcli-linux-arm64"
-      sha256 "4fb232f3c53d01305b3b633cbb34e1b5f054b301e6181a0b8fa2b80918cc9549"
+      sha256 "6f84a3dff99bf1eddfe2db1bfe6311849a43fcae33f505882d33071d40391ed9"
     end
     on_intel do
       url "https://github.com/dgrr/tgcli/releases/download/v#{version}/tgcli-linux-amd64"
-      sha256 "6068414b10a5c4df8ff855cd4d7de25a78744f1835b45c12acd6a9ef482258a9"
+      sha256 "bcba33db43a2c86b63f7e0baa59b61c0965378b02058e7d444ea3a0d8e8a7ed5"
     end
   end
 
   def install
     binary = Dir["tgcli-*"].first || "tgcli"
     bin.install binary => "tgcli"
+  end
+
+  def caveats
+    <<~EOS
+      If you have a tgcli sync service running, restart it to use the new version:
+        launchctl kickstart -k gui/$(id -u)/com.tgcli.sync
+
+      Or manually:
+        launchctl stop com.tgcli.sync
+        launchctl start com.tgcli.sync
+    EOS
   end
 
   test do
